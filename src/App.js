23ea13dc78c3ab8a2cbdf8f6490e870bc3c858fe
Yubin <yubiny16@gmail.com>
2021-12-19 22:26:52 +0900

    responsive design done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
-import {ReactComponent as EcosystemDiagram} from './res/ecosystem_diagram.svg';
-import {ReactComponent as RoadmapDiagram} from './res/roadmap_diagram.svg';
-import {ReactComponent as FeaturesDiagram} from './res/features_diagram.svg';
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-scroll";
+// import {ReactComponent as EcosystemDiagram} from './res/ecosystem_diagram.svg';
+// import {ReactComponent as RoadmapDiagram} from './res/roadmap_diagram.svg';
+// import {ReactComponent as FeaturesDiagram} from './res/features_diagram.svg';
+import EcosystemDiagram from './res/ecosystem_diagram.svg';
+import RoadmapDiagram from './res/roadmap_diagram.svg';
+import FeaturesDiagram from './res/features_diagram.svg';
 import ShibaTrio from './res/shiba_trio.png';
 import LargeLogo from './res/logo_large.png';
 import SmallLogo from './res/logo_small.png';
@@ -9,32 +14,59 @@ import DiscordLogo from './res/discord_logo.png';
 import TwitterLogo from './res/twitter_logo.png';
 
 function App() {
+  const [width, setWindowWidth] = useState(0)
+   useEffect(() => { 
+
+     updateDimensions();
+
+     window.addEventListener("resize", updateDimensions);
+     return () => 
+       window.removeEventListener("resize",updateDimensions);
+    }, [])
+    const updateDimensions = () => {
+      const width = window.innerWidth
+      setWindowWidth(width)
+    }
+
   return (
     <div className="app">
       <div className="header">
-        <div className="header-logo">
-          <img src={SmallLogo} alt="Shiba Run Logo" />
-        </div>
-        <div className="header-links">
-          <a href="#welcome">Welcome</a>
-          {/* <a href="#features">Features</a>
-          <a href="#ecosystem">Ecosystem</a>
-          <a href="#roadmap">Roadmap</a>
-          <a href="#Community">Community</a>
-          <a href="#">Whitepaper</a> */}
-        </div>
+        <Link className="header-logo" activeClass="active" to="main" spy={true} smooth={true}
+          offset={-100} duration={500}><img src={SmallLogo} alt="Shiba Run Logo"/>
+        </Link>
+        {width > 1024 ?
+          <div className="header-links">
+            <Link className="header-item" activeClass="active" to="features" spy={true} smooth={true} offset={-100}duration={500}>Features</Link>
+            <Link className="header-item" activeClass="active" to="ecosystem" spy={true} smooth={true} offset={-100} duration={500} >Ecosystem</Link>
+            <Link className="header-item" activeClass="active" to="roadmap" spy={true} smooth={true} offset={-100} duration={500}>Roadmap</Link>
+            <img className="header-item header-social-btn" src={DiscordLogo} alt="Discord Btn" />
+            <img className="header-item header-social-btn" src={TwitterLogo} alt="Twitter Btn" />
+            <button className="header-item whitepaper-btn" href="https://yubiny16.gitbook.io/shiba-run-whitepaper/">Whitepaper</button>
+          </div> :
+          <div className="header-links">
+            <img className="header-item header-item-mobile header-social-btn" src={DiscordLogo} alt="Discord Btn" />
+            <img className="header-item header-item-mobile header-social-btn" src={TwitterLogo} alt="Twitter Btn" />
+            <button className="whitepaper-btn header-item header-item-mobile  whitepaper-btn-mobile" href="https://yubiny16.gitbook.io/shiba-run-whitepaper/">Whitepaper</button>
+          </div>
+        }
       </div>
-      <div className="welcome-container">
-        <div className="welcome-content-wrapper welcome-text-wrapper">
-          <div className="welcome-logo-wrapper">
+      <div id="main" className="main-container">
+        <div className="main-content-wrapper main-text-wrapper">
+          <div className="main-logo-wrapper">
             <img src={LargeLogo} alt="Shiba Run Logo" />
           </div>
-          <div className="welcome-descriptions">
-            <p>Shibarun is a digital dog racing P2E game where anyone can own, race, and breed NFT Shibas!</p>
-            <button>MINT SOON!</button>
-          </div>
+            {width > 1024 ? 
+              <div className="main-description">
+                <p className="main-paragraph">Shibarun is a digital dog racing P2E game where anyone can own, race, and breed NFT Shibas!</p>
+                <button className="primary-btn">MINT SOON!</button>
+              </div> :
+              <div className="main-description main-description-mobile">
+                <p className="main-paragraph main-paragraph-mobile">Shibarun is a digital dog racing P2E game where anyone can own, race, and breed NFT Shibas!</p>
+                <button className="primary-btn primary-btn-mobile">MINT SOON!</button>
+              </div>
+            }
         </div>
-        <div className="welcome-content-wrapper">
+        <div className="main-content-wrapper">
           <img src={ShibaTrio} alt="Image of three shibas" />
         </div>
       </div>
@@ -43,21 +75,42 @@ function App() {
           <img src={ShibaTrio} alt="Image of three shibas" />
         </div>
         <div className="welcome-content-wrapper welcome-text-wrapper">
-          <div className="welcome-descriptions">
-            <h2>WELCOME!</h2>
-            <p>
-              Shibarun is a play to earn (P2E)
-              tokens based on their game play
-              tokens based on their was she all
-              their meme-inspired non-fungible
-              token (NFT) Shibas.
-            </p>
-          </div>
+          {width > 1024 ? 
+            <div className="welcome-descriptions">
+              <h2 className="welcome-section-title">WELCOME!</h2>
+              <p className="welcome-paragraph">
+                Shibarun is a play to earn (P2E)
+                tokens based on their game play
+                tokens based on their was she all
+                their meme-inspired non-fungible
+                token (NFT) Shibas.
+              </p> 
+            </div>:
+            <div className="welcome-descriptions">
+              <h2 className="welcome-section-title welcome-section-title-mobile">WELCOME!</h2>
+              <p className="welcome-paragraph welcome-paragraph-mobile">
+                Shibarun is a play to earn (P2E)
+                tokens based on their game play
+                tokens based on their was she all
+                their meme-inspired non-fungible
+                token (NFT) Shibas.
+              </p> 
+            </div>
+          }
         </div>
       </div>
-      <FeaturesDiagram id="features" />
-      <EcosystemDiagram id="ecosystem" />
-      <RoadmapDiagram id="roadmap" />
+      <div id="features" className="features-container">
+        <img src={FeaturesDiagram} alt="Features Diagram" />
+        {/* <FeaturesDiagram id="features" /> */}
+      </div>
+      <div id="ecosystem" className="ecosystem-container">
+        <img src={EcosystemDiagram} alt="Features Diagram" />
+        {/* <EcosystemDiagram id="ecosystem" /> */}
+      </div>
+      <div id="roadmap" className="roadmap-container">
+        <img src={RoadmapDiagram} alt="Features Diagram" />
+        {/* <RoadmapDiagram id="roadmap" /> */}
+      </div>
       <div id="community" className="community">
         <div className="community-logo-wrapper">
           <img src={LargeLogo} alt="Shiba Run Logo" />
